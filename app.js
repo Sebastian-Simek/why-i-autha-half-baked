335e95c7c5833c9674d361a73f703b42dd25222f
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-05-16 11:59:51 -0700

    added signInUser function and event listener

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { signupUser } from './fetch-utils.js';
+import { signupUser, signInUser } from './fetch-utils.js';
 
 const signInForm = document.getElementById('sign-in');
 const signInEmail = document.getElementById('sign-in-email');
@@ -11,6 +11,15 @@ const signUpPassword = document.getElementById('sign-up-password');
 // Wire up sign in and sign up forms to supabase
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
+signInForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signInForm);
+    const user = await signInUser(data.get('email'), data.get('password'));
+    if (user) {
+        location.replace('./other-page');
+    }
+});
+
 signUpForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const data = new FormData(signUpForm);
